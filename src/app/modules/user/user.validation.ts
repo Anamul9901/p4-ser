d538b71f3a3179d0f9646c632340d7fbdc613636
Anamul Haque <anamulhaque9901@gmail.com>
2024-07-09 00:22:41 +0600

    use zod validation

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+
+const signInValidationSchema = z.object({
+  body: z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z
+      .string({
+        invalid_type_error: 'Password must be a string',
+      })
+      .max(20, { message: 'Password can not be more than 20 characters' }),
+    role: z.enum(['admin', 'user']),
+  }),
+});
+
+const loginValidationSchema = z.object({
+  body: z.object({
+    email: z.string({ required_error: 'Email is required!' }),
+    password: z.string({ required_error: 'Password is required!' }),
+  }),
+});
+
+export const UserValidation = {
+  signInValidationSchema,
+  loginValidationSchema,
+};
