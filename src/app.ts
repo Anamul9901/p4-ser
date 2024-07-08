738acd3611b3e62b912cc961bbcc87ea44dce9b6
Anamul Haque <anamulhaque9901@gmail.com>
2024-07-09 00:39:53 +0600

    user modules created

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import router from './app/routes';
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api', router)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,6 +1,20 @@
 import express from 'express';
-
+import validateRequest from '../../middlwares/validateRequest';
+import { UserValidation } from './user.validation';
+import { UserControllers } from './user.controller';
 
 const router = express.Router();
 
-router.post('/signup')
+router.post(
+  '/signup',
+  validateRequest(UserValidation.signInValidationSchema),
+  UserControllers.signUpUser
+);
+
+router.post(
+  '/login',
+  validateRequest(UserValidation.loginValidationSchema),
+  UserControllers.loginUser
+);
+
+export const UserRoutes = router;
